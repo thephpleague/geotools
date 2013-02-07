69a7cac556fed01936e918e3bcc94ae2263c590a
Antoine Corcy <dev@sbin.dk>
2013-02-07 15:50:55 +0100

    Added: test to Geotools class

--- tests/Geotools/Tests/GeotoolsTest.php
+++ tests/Geotools/Tests/GeotoolsTest.php
@@ -11,6 +11,10 @@
 
 namespace Geotools\Tests;
 
+use Geotools\Geotools;
+use Geotools\Coordinate\Coordinate;
+use Geocoder\Geocoder;
+
 /**
  * Geotools test class
  *
@@ -18,11 +22,76 @@ namespace Geotools\Tests;
 */
 class GeotoolsTest extends TestCase
 {
-    /**
-     * Test geotools method
-     */
-    public function testGeotools()
+    protected $geotools;
+    protected $from;
+    protected $to;
+
+    protected function setUp()
     {
-        $this->markTestIncomplete('TODO');
+        $this->geotools = new TestableGeotools();
+        $this->from = new MockCoordinate();
+        $this->to = new MockCoordinate();
+    }
+
+    public function testFromValueShouldBeACoordinateInterface()
+    {
+        $this->geotools->from($this->from);
+        $this->assertInstanceOf('Geotools\Coordinate\CoordinateInterface', $this->geotools->getFrom());
+    }
+
+    public function testFromShouldReturnTheSameGeotoolsInstance()
+    {
+        $this->assertSame($this->geotools, $this->geotools->from($this->from));
+    }
+
+    public function testToValueShouldBeACoordinateInterface()
+    {
+        $this->geotools->to($this->to);
+        $this->assertInstanceOf('Geotools\Coordinate\CoordinateInterface', $this->geotools->getTo());
+    }
+
+    public function testToShouldReturnTheSameGeotoolsInstance()
+    {
+        $this->assertSame($this->geotools, $this->geotools->to($this->to));
+    }
+
+    public function testDistanceShouldReturnANewDistanceInstance()
+    {
+        $this->geotools->from($this->from);
+        $this->geotools->to($this->to);
+        $this->assertInstanceOf('Geotools\Distance\DistanceInterface', $this->geotools->distance());
+    }
+
+    public function testPointShouldReturnANewPointInstance()
+    {
+        $this->geotools->from($this->from);
+        $this->geotools->to($this->to);
+        $this->assertInstanceOf('Geotools\Point\PointInterface', $this->geotools->point());
+    }
+
+    public function testBatchShouldReturnANewBatchInstance()
+    {
+        $geocoder = new Geocoder();
+        $this->assertInstanceOf('Geotools\Batch\BatchInterface', $this->geotools->batch($geocoder));
+    }
+}
+
+class TestableGeotools extends Geotools
+{
+    public function getFrom()
+    {
+        return $this->from;
+    }
+
+    public function getTo()
+    {
+        return $this->to;
+    }
+}
+
+class MockCoordinate extends Coordinate
+{
+    public function __construct()
+    {
     }
 }
