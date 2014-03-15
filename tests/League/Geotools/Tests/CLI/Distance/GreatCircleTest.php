8614b49569fef40c61a64e205c89afb67209edb6
Antoine Corcy <dev@sbin.dk>
2014-03-15 17:17:31 +0100

    add great circle to CLI + tests

---
+++ tests/League/Geotools/Tests/CLI/Distance/GreatCircleTest.php
@@ -0,0 +1,154 @@
+<?php
+
+/**
+ * This file is part of the Geotools library.
+ *
+ * (c) Antoine Corcy <contact@sbin.dk>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace League\Geotools\Tests\CLI\Distance;
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+use League\Geotools\Tests\TestCase;
+use League\Geotools\CLI\Distance\GreatCircle;
+
+/**
+ * @author Antoine Corcy <contact@sbin.dk>
+ */
+class GreatCircleTest extends TestCase
+{
+    protected $application;
+    protected $command;
+    protected $commandTester;
+
+    protected function setUp()
+    {
+        $this->application = new Application();
+        $this->application->add(new GreatCircle());
+
+        $this->command = $this->application->find('distance:great-circle');
+
+        $this->commandTester = new CommandTester($this->command);
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     * @expectedExceptionMessage Not enough arguments.
+     */
+    public function testExecuteWithoutArguments()
+    {
+        $this->commandTester->execute(array(
+            'command' => $this->command->getName(),
+        ));
+    }
+
+    /**
+     * @expectedException League\Geotools\Exception\InvalidArgumentException
+     * @expectedExceptionMessage It should be a valid and acceptable ways to write geographic coordinates !
+     */
+    public function testExecuteInvalidArguments()
+    {
+        $this->commandTester->execute(array(
+            'command'     => $this->command->getName(),
+            'origin'      => 'foo, bar',
+            'destination' => ' ',
+        ));
+    }
+
+    public function testExecuteWithoutOptions()
+    {
+        $this->commandTester->execute(array(
+            'command'     => $this->command->getName(),
+            'origin'      => '40° 26.7717, -79° 56.93172',
+            'destination' => '30°16′57″N 029°48′32″W',
+        ));
+
+        $this->assertTrue(is_string($this->commandTester->getDisplay()));
+        $this->assertRegExp('/4625820\.4879867/', $this->commandTester->getDisplay());
+    }
+
+    public function testExecuteWithKmOption()
+    {
+        $this->commandTester->execute(array(
+            'command'     => $this->command->getName(),
+            'origin'      => '40° 26.7717, -79° 56.93172',
+            'destination' => '30°16′57″N 029°48′32″W',
+            '--km'        => true,
+        ));
+
+        $this->assertTrue(is_string($this->commandTester->getDisplay()));
+        $this->assertRegExp('/4625\.8204879867/', $this->commandTester->getDisplay());
+    }
+
+    public function testExecuteWithMileOption()
+    {
+        $this->commandTester->execute(array(
+            'command'     => $this->command->getName(),
+            'origin'      => '40° 26.7717, -79° 56.93172',
+            'destination' => '30°16′57″N 029°48′32″W',
+            '--mi'        => 'true',
+        ));
+
+        $this->assertTrue(is_string($this->commandTester->getDisplay()));
+        $this->assertRegExp('/2874\.3515916962/', $this->commandTester->getDisplay());
+    }
+
+    public function testExecuteWithFtOption()
+    {
+        $this->commandTester->execute(array(
+            'command'     => $this->command->getName(),
+            'origin'      => '40° 26.7717, -79° 56.93172',
+            'destination' => '30°16′57″N 029°48′32″W',
+            '--ft'        => 'true',
+        ));
+
+        $this->assertTrue(is_string($this->commandTester->getDisplay()));
+        $this->assertRegExp('/15176576\.404156/', $this->commandTester->getDisplay());
+    }
+
+    /**
+     * @expectedException League\Geotools\Exception\InvalidArgumentException
+     * @expectedExceptionMessage Please provide an ellipsoid name !
+     */
+    public function testExecuteWithEmptyEllipsoidOption()
+    {
+        $this->commandTester->execute(array(
+            'command'     => $this->command->getName(),
+            'origin'      => '40° 26.7717, -79° 56.93172',
+            'destination' => '30°16′57″N 029°48′32″W',
+            '--ellipsoid' => ' ',
+        ));
+    }
+
+    /**
+     * @expectedException League\Geotools\Exception\InvalidArgumentException
+     * @expectedExceptionMessage foo ellipsoid does not exist in selected reference ellipsoids !
+     */
+    public function testExecuteWithoutAvailableEllipsoidOption()
+    {
+        $this->commandTester->execute(array(
+            'command'     => $this->command->getName(),
+            'origin'      => '40° 26.7717, -79° 56.93172',
+            'destination' => '30°16′57″N 029°48′32″W',
+            '--ellipsoid' => 'foo',
+        ));
+    }
+
+    public function testExecuteWithEllipsoidOption_BESSEL_1841_NAMBIA()
+    {
+        $this->commandTester->execute(array(
+            'command'     => $this->command->getName(),
+            'origin'      => '40° 26.7717, -79° 56.93172',
+            'destination' => '30°16′57″N 029°48′32″W',
+            '--ellipsoid' => 'BESSEL_1841_NAMBIA',
+            '--mi'        => true,
+        ));
+
+        $this->assertTrue(is_string($this->commandTester->getDisplay()));
+        $this->assertRegExp('/2874\.0577024979/', $this->commandTester->getDisplay());
+    }
+}
