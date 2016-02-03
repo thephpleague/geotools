4e2149f05990c57a0ca09b623e8bdafff954681a
Antoine Corcy <dev@sbin.dk>
2016-02-03 17:33:03 +0100

    bump version to 0.7.1-dev

--- src/Geotools.php
+++ src/Geotools.php
@@ -30,7 +30,7 @@ class Geotools extends AbstractGeotools implements GeotoolsInterface
      * Version.
      * @see http://semver.org/
      */
-    const VERSION = '0.7.0';
+    const VERSION = '0.7.1-dev';
 
 
     /**
