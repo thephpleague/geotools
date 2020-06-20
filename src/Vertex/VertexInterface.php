585e8bb4c9b3c2b014013e3993edf8ecd2c3b2c4
Theodore Messinezis <theo@theomessin.com>
2020-06-21 00:30:04 +0100

    Move all Vertex methods to VertexInterface

--- src/Vertex/VertexInterface.php
+++ src/Vertex/VertexInterface.php
@@ -65,4 +65,92 @@ interface VertexInterface
      * @return integer
      */
     public function getOrdinateIntercept();
+
+    /**
+     * @return integer
+     */
+    public function getPrecision();
+
+    /**
+     * @param  integer $precision
+     * @return $this
+     */
+    public function setPrecision($precision);
+
+    /**
+     * Returns the initial bearing from the origin coordinate
+     * to the destination coordinate in degrees.
+     *
+     * @return float The initial bearing in degrees
+     */
+    public function initialBearing();
+
+    /**
+     * Returns the final bearing from the origin coordinate
+     * to the destination coordinate in degrees.
+     *
+     * @return float The final bearing in degrees
+     */
+    public function finalBearing();
+
+    /**
+     * Returns the initial cardinal point / direction from the origin coordinate to
+     * the destination coordinate.
+     * @see http://en.wikipedia.org/wiki/Cardinal_direction
+     *
+     * @return string The initial cardinal point / direction
+     */
+    public function initialCardinal();
+
+    /**
+     * Returns the final cardinal point / direction from the origin coordinate to
+     * the destination coordinate.
+     * @see http://en.wikipedia.org/wiki/Cardinal_direction
+     *
+     * @return string The final cardinal point / direction
+     */
+    public function finalCardinal();
+
+    /**
+     * Returns the half-way point / coordinate along a great circle
+     * path between the origin and the destination coordinates.
+     *
+     * @return CoordinateInterface
+     */
+    public function middle();
+
+    /**
+     * Returns the destination point with a given bearing in degrees travelling along a
+     * (shortest distance) great circle arc and a distance in meters.
+     *
+     * @param integer $bearing  The bearing of the origin in degrees.
+     * @param integer $distance The distance from the origin in meters.
+     *
+     * @return CoordinateInterface
+     */
+    public function destination($bearing, $distance);
+
+    /**
+     * Returns true if the vertex passed on argument is on the same line as this object
+     *
+     * @param  Vertex  $vertex The vertex to compare
+     * @return boolean
+     */
+    public function isOnSameLine(Vertex $vertex);
+
+    /**
+     * Returns the other coordinate who is not the coordinate passed on argument
+     * @param  CoordinateInterface $coordinate
+     * @return null|Coordinate
+     */
+    public function getOtherCoordinate(CoordinateInterface $coordinate);
+
+    /**
+     * Returns the determinant value between $this (vertex) and another vertex.
+     *
+     * @param  Vertex $vertex [description]
+     * @return [type]         [description]
+     */
+    public function getDeterminant(Vertex $vertex);
+
 }
